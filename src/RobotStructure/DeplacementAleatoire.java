efffb35c2b6a84c0ec638405e50d7a568e0fa747
Xanthor-IV <hugo-simon.france@hotmail.fr>
2019-02-26 16:07:07 +0100

    Update DeplacementAleatoire.java

--- src/RobotStructure/DeplacementAleatoire.java
+++ src/RobotStructure/DeplacementAleatoire.java
@@ -1,26 +1,64 @@
 package RobotStructure;
+import java.util.Random;
 
 public class DeplacementAleatoire extends AlgoDeplacement{
 
+	private double finalAngle;
 	
-	public void nextMove(Robot r, double dt) {
+	public DeplacementAleatoire(Robot parent){
+		this.rob = parent;
+		this.state = MovingState.NOMOVE;
+	}
+	
+	public DeplacementAleatoire(Robot parent, double _lspeed, double _rspeed){
+		this.rob = parent;
+		this.state = MovingState.NOMOVE;
+		this.lSpeed = _lspeed;
+		this.rSpeed = _rspeed;
+	}
+	
+	public DeplacementAleatoire(Robot parent, double _lspeed, double _rspeed, MovingState _state){
+		this.rob = parent;
+		this.state = _state;
+		this.lSpeed = _lspeed;
+		this.rSpeed = _rspeed;
+	}
+	
+	public DeplacementAleatoire(Robot parent, MovingState _state){
+		this.rob = parent;
+		this.state = _state;
+	}
+	
+	public void nextMove(double dt) 
+	{
 		
 		if(state.equals(MovingState.LINEAR))
 		{
 			double dl = dt*lSpeed;
 			double dr = dt*lSpeed;
-			r.moveRobot(dl, dr);
+			rob.moveRobot(dl, dr);
 		}
 		else if(state.equals(MovingState.ROTATION))
 		{
-			double dr = dt*rSpeed*r.getDiametre();
-			double dl = - dt*rSpeed*r.getDiametre();
-			r.moveRobot(dl, dr);
+			if( Math.abs(finalAngle - rob.getAngle()) > 2 * dt * rSpeed  )
+			{
+				double dr = dt*rSpeed*rob.getDiametre();
+				double dl = - dt*rSpeed*rob.getDiametre();
+				rob.moveRobot(dl, dr);
+			}
+			else
+				setMoveType(MovingState.LINEAR);
 		}
-		else
+		else // NOMOVE
 		{
-			//Nothing
+			// Nothing
 		}
 	}
 	
+	public void obstacleDetected(double angle)
+	{
+		Random r = new Random();
+		finalAngle = 0 + (Math.PI - 0) * r.nextDouble() + rob.getAngle();
+		setMoveType(MovingState.ROTATION);
+	}
 }
